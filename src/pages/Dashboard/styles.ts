53e0b0d8251a0d6421ecb091439b1c52261701bf
dantls <dan_tls@hotmail.com>
2021-02-20 23:55:53 -0400

    add responsiveness

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const FoodsContainer = styled.div`
   width: 100%;
-  max-width: 1280px;
+  max-width: 1440px;
   margin: 0 auto;
   padding: 40px 0;
   margin-top: -140px;
@@ -10,5 +10,14 @@ export const FoodsContainer = styled.div`
   display: grid;
 
   grid-template-columns: repeat(3, 1fr);
-  grid-gap: 32px;
+  grid-gap: 10px;
+
+  @media (max-width: 1200px) {
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap: 10px;
+  }
+  @media (max-width: 800px) {
+    grid-template-columns: repeat(1, 1fr);
+    grid-gap: 10px;
+  }
 `;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import { Switch, Route } from 'react-router-dom';
+import Customers from '../pages/Customers';
 
 import Dashboard from '../pages/Dashboard';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Dashboard} />
+    <Route path="/clientes" exact component={Customers} />
   </Switch>
 );
 
